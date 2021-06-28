d0172d2b65f6346fb6ecc45f24b7cf1f8f6c26f0
lzl <2233788867@qq.com>
2021-06-28 15:38:53 +0800

    fix: inaccuracy of the isLayoutDirectionResolved method on SDKs 17 and 18.

--- sliding-drawer-layout/src/main/java/com/liuzhenlin/slidingdrawerlayout/Utils.java
+++ sliding-drawer-layout/src/main/java/com/liuzhenlin/slidingdrawerlayout/Utils.java
@@ -14,12 +14,18 @@ import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author 刘振林
  */
 public class Utils {
     private Utils() {
     }
+
+    private static Method sIsLayoutDirectionResolvedMethod;
+    private static boolean sIsLayoutDirectionResolvedMethodFetched;
 
     public static int dimColor(int color, @FloatRange(from = 0.0f, to = 1.0f) float amount) {
         return roundFloat(((color & 0xff000000) >>> 24) * (1 - amount)) << 24
@@ -52,11 +58,34 @@ public class Utils {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             return view.isLayoutDirectionResolved();
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            // FIXME: returns the exact result as above
-            return view.getWindowToken() != null;
+            ensureIsLayoutDirectionResolvedMethodFetched();
+            if (sIsLayoutDirectionResolvedMethod != null) {
+                try {
+                    Boolean ret = (Boolean) sIsLayoutDirectionResolvedMethod.invoke(view);
+                    return ret != null && ret;
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+            return false;
         }
         // No support for RTL in SDKs below 17
         return true;
+    }
+
+    private static void ensureIsLayoutDirectionResolvedMethodFetched() {
+        if (!sIsLayoutDirectionResolvedMethodFetched) {
+            try {
+                sIsLayoutDirectionResolvedMethod =
+                        View.class.getDeclaredMethod("isLayoutDirectionResolved");
+                sIsLayoutDirectionResolvedMethod.setAccessible(true);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+            sIsLayoutDirectionResolvedMethodFetched = true;
+        }
     }
 
     /**
