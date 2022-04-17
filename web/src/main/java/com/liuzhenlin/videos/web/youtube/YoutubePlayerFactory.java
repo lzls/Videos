b5c71eeda2fbc61fa758dd64e9106575d573a5e4
lzl <2233788867@qq.com>
2022-04-17 08:05:50 +0800

    fix: value of WeakHashMap referring indirectly to its key caused memory leaks.

--- web/src/main/java/com/liuzhenlin/videos/web/youtube/YoutubePlayerFactory.java
+++ web/src/main/java/com/liuzhenlin/videos/web/youtube/YoutubePlayerFactory.java
@@ -12,30 +12,32 @@ import androidx.annotation.Nullable;
 import com.liuzhenlin.common.utils.NonNullApi;
 import com.liuzhenlin.videos.web.player.WebPlayer;
 
+import java.lang.ref.WeakReference;
 import java.util.WeakHashMap;
 
 @NonNullApi
 public class YoutubePlayerFactory {
 
-    private static final WeakHashMap<WebView, WebPlayer> sPlayers = new WeakHashMap<>();
+    private static final WeakHashMap<WebView, WeakReference<WebPlayer>> sPlayers = new WeakHashMap<>();
 
     /**
      * Obtains a WebPlayer used to play YouTube videos on the given WebView based on
      * the current user's preferred UI style for the playback page.
      */
     public static WebPlayer obtain(WebView webView) {
-        @Nullable WebPlayer player = sPlayers.get(webView);
+        @Nullable WeakReference<WebPlayer> playerRef = sPlayers.get(webView);
+        @Nullable WebPlayer player = playerRef == null ? null : playerRef.get();
         switch (Youtube.Prefs.get(webView.getContext()).getPlaybackPageStyle()) {
             case Youtube.Prefs.PLAYBACK_PAGE_STYLE_ORIGINAL:
                 if (!(player instanceof YoutubePlayer)) {
                     player = new YoutubePlayer(webView);
-                    sPlayers.put(webView, player);
+                    sPlayers.put(webView, new WeakReference<>(player));
                 }
                 return player;
             case Youtube.Prefs.PLAYBACK_PAGE_STYLE_BRIEF:
                 if (!(player instanceof YoutubeIFramePlayer)) {
                     player = new YoutubeIFramePlayer(webView);
-                    sPlayers.put(webView, player);
+                    sPlayers.put(webView, new WeakReference<>(player));
                 }
                 return player;
             default:
