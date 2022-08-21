4c6e912903530ce76aa8a51134c892c702a57909
lzl <2233788867@qq.com>
2022-08-21 11:30:01 +0800

    chore: upload shell scripts used for app release

---
+++ shells/release.sh
@@ -0,0 +1,242 @@
+#!/bin/bash
+
+#
+# Created on 2021-8-19 8:51:17 PM.
+# Copyright © 2021 刘振林. All rights reserved.
+#
+
+readonly SHELLS_DIR=$(cd "$(dirname "$0")"; pwd)
+source "$SHELLS_DIR"/utils.sh &&
+  source "$SHELLS_DIR"/git.sh
+verifyLastOpSuccessed
+
+const_int START_TIME=$(date +%s)
+echo "start time: $(date -r $START_TIME '+%a %Y-%m-%d %H:%M:%S %z')"
+
+PROJECTS_ROOT='/Users/liuzhenlin/Android/projects'
+APK_PARTS_COUNT=
+APP_VERSION_NAME=
+bool BETA=$false
+bool AMEND=$false
+
+function __echo_release_sh_usage___() {
+  if [ "$1" ]; then
+    echo "illegal option: $1" >&2
+  fi
+  printf "%s\n\t%s\n\t%s\n" 'usage: ./shells/release.sh --version-name=<app version name>' \
+    '--parts-count=<count of split apk parts> [--beta] [--amend]' \
+    '[--projects-root=<parent dir of the local Videos repo>]'
+}
+
+function __parseShellArgs() {
+  local projectsRootPrefix='--projects-root='
+  local versionNamePrefix='--version-name='
+  local partsCountPrefix='--parts-count='
+  local beta='--beta'
+  local amend='--amend'
+  for arg in "$@"; do
+    case $arg in
+    ${projectsRootPrefix}*) PROJECTS_ROOT=${arg:${#projectsRootPrefix}} ;;
+    ${versionNamePrefix}*) APP_VERSION_NAME=${arg:${#versionNamePrefix}} ;;
+    ${partsCountPrefix}*) APK_PARTS_COUNT=${arg:${#partsCountPrefix}} ;;
+    $beta) BETA=$true ;;
+    $amend) AMEND=$true ;;
+    --help) __echo_release_sh_usage___; exit 0 ;;
+    *) __echo_release_sh_usage___ "$arg"; waitToExit 1 ;;
+    esac
+  done
+
+  test "$APP_VERSION_NAME"
+  verifyLastOpSuccessed "Release version name is not initialized. Use $versionNamePrefix to specify it."
+  test "$APK_PARTS_COUNT"
+  verifyLastOpSuccessed "The count of apk parts to upload is not initialized. Use $partsCountPrefix" \
+    'to specify it.'
+}
+__parseShellArgs "$@"
+
+readonly COMMIT_MSG="Release version $APP_VERSION_NAME"
+
+readonly SERVICE_PROJECTS_ROOT="$PROJECTS_ROOT/tmp"
+readonly APK_SOURCE_DIR="$PROJECTS_ROOT/Videos/app/release"
+
+readonly SSH_APKS_HOLDER_VIDEOS_REPO='git@ApksHolder.github.com:ApksHolder/Videos.git'
+readonly SSH_LZLS_VIDEOS_REPO='git@lzls.github.com:lzls/Videos.git'
+readonly SSH_LZLS_VIDEOS_SERVER_REPO='git@lzls.gitlab.com:lzls/Videos-Server.git'
+
+if [ ! -e "$SERVICE_PROJECTS_ROOT" ]; then
+  # shellcheck disable=SC2003
+  cd "$PROJECTS_ROOT" && mkdir "${SERVICE_PROJECTS_ROOT:$(expr ${#PROJECTS_ROOT} + 1)}"
+  verifyLastOpSuccessed
+fi
+
+function clonePushBranch() {
+  local remote=$1
+  local branch=$2
+  local storedDir=$3
+
+  trace git clone -b "$branch" "$remote" "$storedDir" &&
+    trace cd "$storedDir"
+}
+
+function stashAndRebasePushBranch() {
+  local remote=$1
+  local branch=$2
+  local storedDir=$3
+
+  trace cd "$storedDir" &&
+    trace git stash &&
+    trace git checkout "$branch" &&
+    trace git reset --hard HEAD~0 &&
+    trace git fetch &&
+    trace git rebase "$(git remote)/$branch"
+}
+
+function ensureRepoPushBranchUpToDate() {
+  local remote=$1
+  local branch=$2
+  local storedDir=$3
+
+  if [ ! -d "$storedDir" ] || [ "$(ls -A "$storedDir")" = '' ]; then
+    clonePushBranch "$remote" "$branch" "$storedDir"
+  else
+    stashAndRebasePushBranch "$remote" "$branch" "$storedDir"
+  fi
+}
+
+function __force() {
+  if [ $AMEND -eq $true ]; then
+    echo '--force'
+  else
+    echo ''
+  fi
+}
+
+function commitAndPush() {
+  local commitMsg=${1:-$COMMIT_MSG}
+  local remote=${2:-origin}
+  shift 2
+  local branches=${*:-master}
+
+  if [ $AMEND -eq $true ]; then
+    trace gitAmendCommit --m="$commitMsg"
+  else
+    trace gitCommit "$commitMsg"
+  fi
+  verifyLastOpSuccessed --pause-only
+  # shellcheck disable=SC2046,SC2116
+  trace git push "$remote" $(echo "$branches") $(__force)
+}
+
+
+echoBoldBlueText '\nVerifying the apk parts are split from the current app-release.apk'
+int i=1
+# shellcheck disable=SC2004
+while (($i <= $APK_PARTS_COUNT)); do
+  if [ $i -eq 1 ]; then
+      cat "$APK_SOURCE_DIR"/app-release$i.apk > "$APK_SOURCE_DIR"/_app-release.apk
+  else
+      cat "$APK_SOURCE_DIR"/app-release$i.apk >> "$APK_SOURCE_DIR"/_app-release.apk
+  fi
+  ((i = $i + 1))
+done
+test "$(shasum "$APK_SOURCE_DIR"/app-release.apk | awk '{ print $1 }')" = \
+     "$(shasum "$APK_SOURCE_DIR"/_app-release.apk | awk '{ print $1 }')"
+int exitCode=$?
+rm -f "$APK_SOURCE_DIR"/_app-release.apk
+if [ $exitCode -ne 0 ]; then
+  verifyLastOpSuccessed --exit-code=$exitCode
+fi
+
+
+echoBoldBlueText "\nVerifying tag v$APP_VERSION_NAME exists in the commit tree for the dev branch of" \
+  "$PROJECTS_ROOT/Videos"
+
+cdOrWaitToExit "$PROJECTS_ROOT"/Videos
+
+test "$(git branch --show-current)" = 'dev'
+verifyLastOpSuccessed 'Current branch is not dev'
+
+readonly RELEASE_TAG_COMMIT_ID=$(git rev-parse "v$APP_VERSION_NAME")
+echo "$RELEASE_TAG_COMMIT_ID" | grep -E '^[[:xdigit:]]{40}$' >/dev/null &&
+  git log --pretty=reference | grep "${RELEASE_TAG_COMMIT_ID:0:7}"
+verifyLastOpSuccessed "git tag v$APP_VERSION_NAME does not exist in the commit tree"
+
+
+echoBoldBlueText "\nEnsuring $PROJECTS_ROOT/Videos is up-to-date on its release branch..."
+ensureRepoPushBranchUpToDate $SSH_LZLS_VIDEOS_REPO release "$PROJECTS_ROOT"/Videos &&
+  if [ $AMEND -eq $true ] \
+      && git log --pretty=oneline \
+          | grep "Merge tag \('\|\"\)v$APP_VERSION_NAME\('\|\"\)" >/dev/null; then
+    trace git reset --hard HEAD~1
+  fi &&
+  trace git merge "v$APP_VERSION_NAME" --no-ff --no-edit &&
+  if [ $AMEND -eq $true ]; then
+    _last_commit=$(git remote)/release
+    trace gitAmendCommit --date="$(gitGetFormattedLog --format='%ad' --head="$_last_commit" -1)" \
+                         --commit-date="$(gitGetFormattedLog --format='%cd' --head="$_last_commit" -1)"
+  fi &&
+  trace git checkout dev
+verifyLastOpSuccessed
+
+echoBoldBlueText "\nEnsuring $SERVICE_PROJECTS_ROOT/Videos-master is up-to-date on its push branch..."
+ensureRepoPushBranchUpToDate $SSH_LZLS_VIDEOS_REPO master "$SERVICE_PROJECTS_ROOT"/Videos-master
+verifyLastOpSuccessed
+
+echoBoldBlueText "\nEnsuring $SERVICE_PROJECTS_ROOT/Videos is up-to-date on its push branch..."
+ensureRepoPushBranchUpToDate $SSH_APKS_HOLDER_VIDEOS_REPO master "$SERVICE_PROJECTS_ROOT"/Videos
+verifyLastOpSuccessed
+
+echoBoldBlueText "\nEnsuring $SERVICE_PROJECTS_ROOT/Videos-Server is up-to-date on its push branch..."
+ensureRepoPushBranchUpToDate $SSH_LZLS_VIDEOS_SERVER_REPO master "$SERVICE_PROJECTS_ROOT"/Videos-Server
+verifyLastOpSuccessed
+
+
+echoBoldBlueText '\nPushing data of app upgrade to https://github.com/ApksHolder/Videos.git...'
+cd "$SERVICE_PROJECTS_ROOT"/Videos &&
+  if [ $BETA -ne $true ]; then fcp "$APK_SOURCE_DIR"/app-release.apk ./; fi &&
+  fcp "$PROJECTS_ROOT"/Videos/app.json ./ &&
+  git add ./app-release.apk ./app.json &&
+  commitAndPush "$COMMIT_MSG" $SSH_APKS_HOLDER_VIDEOS_REPO master master:release
+verifyLastOpSuccessed --pause-only
+
+echoBoldBlueText '\nPushing the new release tag and the latest code to dev and release branches of' \
+  'https://github.com/lzls/Videos.git...'
+cd "$PROJECTS_ROOT"/Videos &&
+  trace git push $SSH_LZLS_VIDEOS_REPO tags/"v$APP_VERSION_NAME" \
+    dev:dev release:release $(__force)
+verifyLastOpSuccessed --pause-only
+
+echoBoldBlueText '\nPushing information of app upgrade to the master branch of' \
+  'https://github.com/lzls/Videos.git...'
+cd "$SERVICE_PROJECTS_ROOT"/Videos-master &&
+  fcp "$PROJECTS_ROOT"/Videos/app.json ./ &&
+  git add ./app.json &&
+  commitAndPush "$COMMIT_MSG" $SSH_LZLS_VIDEOS_REPO master
+verifyLastOpSuccessed --pause-only
+
+echoBoldBlueText '\nPushing data of app upgrade to https://gitlab.com/lzls/Videos-Server.git...' \
+  'This will be automatically mirrored to https://gitee.com/lzl_s/Videos-Server.git and' \
+  'https://gitee.com/lzl_s/Videos-Service.git by the GitLab server.'
+
+cdOrWaitToExit "$SERVICE_PROJECTS_ROOT"/Videos-Server/app/Android
+
+if [ $BETA -ne $true ]; then
+  i=1
+  # shellcheck disable=SC2004
+  while (($i <= $APK_PARTS_COUNT)); do
+    fcp "$APK_SOURCE_DIR"/app-release$i.apk ./ &&
+      git add ./app-release$i.apk
+    verifyLastOpSuccessed --pause-only
+    ((i = $i + 1))
+  done
+fi
+
+fcp "$PROJECTS_ROOT"/Videos/app.json ./ &&
+  git add ./app.json &&
+  fcp "$APK_SOURCE_DIR"/app-release.apk ./Videos_v"$APP_VERSION_NAME".apk &&
+  git add ./Videos_v"$APP_VERSION_NAME".apk &&
+  commitAndPush "$COMMIT_MSG" $SSH_LZLS_VIDEOS_SERVER_REPO master
+
+
+# shellcheck disable=SC2046,SC2003
+echo_e "\ncost time: $(date -r $(expr $(date +%s) - $START_TIME) +%M:%S)"
---
+++ shells/utils.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+#
+# Created on 2021-11-23 4:42 PM.
+# Copyright © 2021 刘振林. All rights reserved.
+#
+
+shopt -s expand_aliases 2>/dev/null # Enable alias
+alias fcp='cp -f'
+alias int='declare -i'
+alias const_int='int -r'
+alias bool=int
+alias const_bool=const_int
+
+if ! [ "$false" ]; then
+  const_bool false=1
+fi
+if ! [ "$true" ]; then
+  const_bool true=0
+fi
+
+if ! [ "$isDarwin" ]; then
+  [[ "$(uname)" == Darwin* ]]
+  const_bool isDarwin=$?
+fi
+
+if ! [ "$SHELL_TYPE" ]; then
+  readonly SHELL_TYPE_SH='sh'
+  readonly SHELL_TYPE_BASH='bash'
+  readonly SHELL_TYPE_ZSH='zsh'
+  readonly SHELL_TYPE=$(
+    __shell_regex___='.*(\/|[[:blank:]]|-)([[:alpha:]]*sh)([[:blank:]].*)?'
+    # shellcheck disable=SC2116
+    ps -ef | grep "$(echo $$)" | grep -E "$__shell_regex___" \
+      | sed -r "s/$__shell_regex___/\2/" | head -n 1
+  )
+  echo "shell type: $SHELL_TYPE"
+fi
+
+# shellcheck disable=SC2139
+alias echo_e="$(
+  if [ $isDarwin -eq $true ] && [ "$SHELL_TYPE" = "$SHELL_TYPE_SH" ]; then
+    echo 'echo'
+  else
+    echo 'echo -e'
+  fi
+)"
+
+function echoStyledText() {
+  local text
+  local textColor
+  local bold=0
+  local textPrefix='--text='
+  local textColorPrefix='--text-color='
+  local boldPrefix='--bold='
+  for arg in "$@"; do
+    case ${arg} in
+    ${textPrefix}*) text=${arg:${#textPrefix}} ;;
+    ${textColorPrefix}*) textColor=${arg:${#textColorPrefix}} ;;
+    ${boldPrefix}*) if [ "${arg:${#boldPrefix}}" -eq $true ]; then bold=1; fi ;;
+    esac
+  done
+  echo_e "\033[$bold;${textColor}m${text}\033[0m"
+}
+
+function echoGreenText() {
+  local text="$*"
+  echoStyledText --text="$text" --text-color=32 --bold=$false
+}
+
+function echoBoldBlueText() {
+  local text="$*"
+  echoStyledText --text="$text" --text-color=34 --bold=$true
+}
+
+function echoBoldRedText() {
+  local text="$*"
+  echoStyledText --text="$text" --text-color=31 --bold=$true
+}
+
+# shellcheck disable=SC2162
+function read_p() {
+  printf '%s' "$1"
+  shift 1
+  read "$@"
+}
+
+if [ ! $__can_pause___ ]; then
+  bool __can_pause___=$true
+fi
+function pause() {
+  if [ $__can_pause___ -ne $false ]; then
+    read_p "${1:-Press any key to continue...}"
+  fi
+}
+
+function waitToExit() {
+  local canPause=$__can_pause___
+  __can_pause___=$true
+  pause 'Press any key to exit...'
+  __can_pause___=$canPause
+  exit $1
+}
+
+function cdOrWaitToExit() {
+  cd "$1" || waitToExit $?
+}
+
+function verifyLastOpSuccessed() {
+  local exitCode=$?
+  local errMsg=
+  local pauseOnly=$false
+
+  local pauseOnlyPrefix='--pause-only'
+  local exitCodePrefix='--exit-code='
+  for arg in "$@"; do
+    case ${arg} in
+    $pauseOnlyPrefix)
+      pauseOnly=$true
+      continue ;;
+    ${exitCodePrefix}*)
+      exitCode=${arg:${#exitCodePrefix}}
+      continue ;;
+    esac
+    if [ "$errMsg" ]; then
+      errMsg="$errMsg $arg"
+    else
+      errMsg=$arg
+    fi
+  done
+
+  if [ $exitCode -ne 0 ]; then
+    if [ "$errMsg" ]; then
+      echoBoldRedText "$errMsg" >&2
+    else
+      echoBoldRedText 'Something might go wrong. Thread is suspended now.' \
+        "Expected exit code 0, but got $exitCode." >&2
+    fi
+    if [ $pauseOnly -eq $true ]; then
+      pause 'Press any key to continue...'
+    else
+      pause 'Press any key to exit...'
+      exit $exitCode
+    fi
+  fi
+}
+
+function trace() {
+  local cmd=
+  for arg in "$@"; do
+    if [ "$cmd" ]; then
+      if echo "$arg" | grep ' ' >/dev/null; then
+        cmd="$cmd \"$arg\""
+      else
+        cmd="$cmd $arg"
+      fi
+    else
+      cmd=$arg
+    fi
+  done
+  echoGreenText ">>>>  $cmd"
+
+  "$@"
+}
