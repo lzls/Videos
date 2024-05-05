b014bec032b4e10dd34263e071bf83d694bbece7
lzl <2233788867@qq.com>
2024-05-05 13:36:49 +0800

    docs: add comments to the compilation scripts for exoplayer extensions

--- shells/compile_exo_ext_av1.sh
+++ shells/compile_exo_ext_av1.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# Set the following environment variables.
 trace cd "$EXO_PLAYER_ROOT" &&
   AV1_MODULE_PATH="$(pwd)/extensions/av1/src/main"
 verifyLastOpSuccessed
 
+# Fetch cpu_features library.
 trace cd "${AV1_MODULE_PATH}/jni" &&
   ensureRepoUpdateToDate https://github.com/google/cpu_features cpu_features --clean
 verifyLastOpSuccessed
 
+# Fetch libgav1.
 trace cd "${AV1_MODULE_PATH}/jni" &&
   ensureRepoUpdateToDate https://chromium.googlesource.com/codecs/libgav1 libgav1 --clean
 verifyLastOpSuccessed
 
+# Fetch Abseil.
 trace cd "${AV1_MODULE_PATH}/jni/libgav1" &&
   ensureRepoUpdateToDate https://github.com/abseil/abseil-cpp.git third_party/abseil-cpp --clean
